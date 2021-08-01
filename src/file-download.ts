2962bcb115354551f0c8da1e69adad0c0fe93de9
Anton <noten.b@hotmail.com>
2021-08-01 21:03:18 +0200

    📽 start of video conversion

--- src/file-download.ts
+++ src/file-download.ts
@@ -4,8 +4,8 @@ import fs = require('fs');
 export function downloadFile(url: string, path: string): void {
     
     const file = fs.createWriteStream(path);
-    const request = http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
-    response.pipe(file);
+    const request = https.get(url, function(response) {
+        response.pipe(file);
     });
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,55 @@
 import * as Discord from 'discord.js'
 import * as fs from 'fs';
+import path = require('path');
+import { downloadFile } from './file-download';
+import {v4 as uuid} from 'uuid';
+import * as ffmpeg from 'ffmpeg';
 
 const client = new Discord.Client();
 const token = fs.readFileSync('token.txt', {encoding: 'utf-8'});
 client.login(token);
 
-client.on('message', (message) => {
-    if (message.content.startsWith("converty"))
+client.on('message', async (message) => {
+    if (message.content.startsWith("converty") == false) {
+        return;
+    }
 
-    if (message.attachments.size > 0) {
-        console.log(message.attachments);
+    if (message.attachments.size == 1) {
+        const attachment = message.attachments.first();
+        if (attachment == null) {
+            return;
+        }
+
+        try {
+            const tempName = uuid();
+            const tempDirPath = "./tempInput";
+            if (fs.existsSync(tempDirPath) == false) {
+                fs.mkdirSync(tempDirPath, {recursive: true});
+            }
+            const tempFilePath = path.join(tempDirPath, tempName + path.extname(attachment.url));
+            downloadFile(attachment.url, tempFilePath);
+
+            // Convert to webp
+            const video = await new ffmpeg(tempFilePath)
+            video.addCommand('-vcodec', 'libwebp');
+            console.log("AAAAAAAAAA");
+            video.addCommand('-lossless', '1');
+            video.addCommand('-preset', 'default');
+            video.addCommand('-loop', '0');
+            video.setDisableAudio();
+            video.addCommand('-vsync', '0');
+            
+            const finishedName = uuid();
+            const finishedDirPath = "./tempOutput";
+            if (fs.existsSync(finishedDirPath) == false) {
+                fs.mkdirSync(finishedDirPath, {recursive: true});
+            }
+            const finishedFilePath = path.join(finishedDirPath, finishedName + '.webp');
+            await video.save(finishedFilePath);
+
+            fs.unlinkSync(tempFilePath);
+        } catch (ex) {
+            console.log(ex);
+        }
     }
 });
